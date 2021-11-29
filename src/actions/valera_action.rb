de75b664a1028978a91eb3c9260510686183307f
DaniilLepekhin <daniillepekhin@yandex.ru>
2021-11-29 12:26:40 +0700

    Actions fix

--- src/actions/valera_action.rb
+++ src/actions/valera_action.rb
@@ -20,7 +20,7 @@ module Actions
       def set_positive
         @valera.positive += @stats['positive'].nil? ? 0 : @stats['positive']
         @valera.positive = @valera.positive >= 10 ? 10 : @valera.positive
-        #@valera.positive = @valera.positive <= -10 ? -10 : @valera.positive
+        # @valera.positive = @valera.positive <= -10 ? -10 : @valera.positive
       end
 
       def set_tiredness
